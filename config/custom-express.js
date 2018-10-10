2d861817e84dbdb84d62d97ed5e5324120616b3d
Victor Mann <victorcmann@gmail.com>
2018-10-10 00:38:10 -0300

    aplicando middlewares com body-parser

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,8 +1,13 @@
 var express = require("express");
 var consign = require("consign");
+var bodyParser = require("body-parser");
 
 module.exports = function () {
     var app = express();
+
+    // aplicando middlewares
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
 
     // auto-loader passando param
     consign()
