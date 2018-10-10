ff90bc7cb9abb3aa23d0b0bd184b4cfae0705b68
Victor Mann <victorcmann@gmail.com>
2018-10-10 12:19:38 -0300

    alterando controller pagamentos para persistir dados

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -9,7 +9,17 @@ module.exports = function (app) {
     // recebendo pagamento
     app.post('/pagamentos/pagamento', function (req, res) {
         let pagamento = req.body;
-        console.log(pagamento);
-        res.send('ok');
+        console.log('processando um requisicao de um novo pagamento');
+
+        pagamento.status = 'CRIADO';
+        pagamento.data = new Date;
+
+        var connection = app.persistencia.connectionFactory();
+        var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+        pagamentoDao.salva(pagamento, function (errs, result) {
+            console.log('pagamento criado: ' + result);
+            res.json(pagamento);
+        });
     });
 }
