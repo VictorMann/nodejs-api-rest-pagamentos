ca8baf1081863ef610b39c348e61ca6c97f32f8f
Victor Mann <victorcmann@gmail.com>
2018-10-15 19:49:21 -0300

    alterando servico memcachedClient para ser manipulado como um module do express

--- servicos/memcachedClient.js
+++ servicos/memcachedClient.js
@@ -1,20 +1,13 @@
 var memcached = require('memcached');
 
-var cliente = new memcached('localhost:11211', {
-    retries: 10,    // tentativas
-    retry: 10000,   // tempo ate a proxima tentativa
-    remove: true    // remove apos limite de tentativas
-});
-
-/*
-cliente.set('pagamento-20', {'id': 20}, 60000, function (erro) {
-    console.log('nova chave adicionada ao cache: pagamento-20');
-});
-*/
-cliente.get('pagamento-20', function (erro, retorno) {
-    if (erro || !retorno) {
-        console.log('MISS - chave nao encontrada');
-    } else {
-        console.log(`HIT - valor: ${JSON.stringify(retorno)}`);
-    }
-});
+// retorna cliente memcached
+function memcachedClient () {
+    return new memcached('localhost:11211', {
+        retries: 10,    // tentativas
+        retry: 10000,   // tempo ate a proxima tentativa
+        remove: true    // remove apos limite de tentativas
+    });
+}
+module.exports = function () {
+    return memcachedClient;
+}
