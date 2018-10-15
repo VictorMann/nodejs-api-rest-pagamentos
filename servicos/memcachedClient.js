0cff210c5d37f21be5c64824d84b736166636a15
Victor Mann <victorcmann@gmail.com>
2018-10-15 18:31:08 -0300

    criando servico memcachedCliente

---
+++ servicos/memcachedClient.js
@@ -0,0 +1,20 @@
+var memcached = require('memcached');
+
+var cliente = new memcached('localhost:11211', {
+    retries: 10,    // tentativas
+    retry: 10000,   // tempo ate a proxima tentativa
+    remove: true    // remove apos limite de tentativas
+});
+
+/*
+cliente.set('pagamento-20', {'id': 20}, 60000, function (erro) {
+    console.log('nova chave adicionada ao cache: pagamento-20');
+});
+*/
+cliente.get('pagamento-20', function (erro, retorno) {
+    if (erro || !retorno) {
+        console.log('MISS - chave nao encontrada');
+    } else {
+        console.log(`HIT - valor: ${JSON.stringify(retorno)}`);
+    }
+});
