7166129a82f4f0a6835a38d5739f162fa64b6338
Victor Mann <victorcmann@gmail.com>
2018-10-16 10:51:08 -0300

    aplicando morgan (log em request) usando o servico de logger c/ winston

--- config/custom-express.js
+++ config/custom-express.js
@@ -2,11 +2,21 @@ var express = require("express");
 var consign = require("consign");
 var bodyParser = require("body-parser");
 var expressValidator = require("express-validator");
+var morgan = require('morgan');
+var logger = require('../servicos/logger');
 
 module.exports = function () {
     var app = express();
 
     // aplicando middlewares
+    app.use(morgan('common', {
+        stream: {
+            write: function (mensagem) {
+                logger.info(mensagem);
+            }
+        }
+    }));
+
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
     app.use(expressValidator());
